ea8d8d42805d36b0b5bba5c45aa40fecc84d874b
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-03 23:05:35 +0530

    03/01/2022

--- app/src/main/java/com/basakjeet08/new_calculator/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/new_calculator/MainActivity.kt
@@ -2,10 +2,34 @@ package com.basakjeet08.new_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start()
+    }
+
+
+
+    //Start of the Program
+    private fun start(){
+        val tvInput : TextView = findViewById(R.id.tvInput)
+        val btnOne : Button = findViewById(R.id.btnOne)
+
+
+
+
+
+
+
+
+
+        btnOne.setOnClickListener{
+
+        }
+
     }
 }
